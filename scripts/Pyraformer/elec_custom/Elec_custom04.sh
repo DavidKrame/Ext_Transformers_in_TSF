6957ba4a8c4e26d57b179f9907271e17523e1dab
David KRAME <kramedavid@gmail.com>
2023-08-19 08:53:22 +0200

    Correction bug -- for - in Pyraformers args

--- scripts/Pyraformer/elec_custom/Elec_custom04.sh
+++ scripts/Pyraformer/elec_custom/Elec_custom04.sh
@@ -15,14 +15,14 @@ file_name="Elec_Custom_04"
 for pred_len in 96 192 384 768
 do
     python -u Pyraformer/long_range_main.py \
-      --data custom \
-      --input_size 96 \
-      --predict_step $pred_len \
-      --root_path ./dataset/ \
-      --file_name $file_name \
-      --data_path custom_elec_04.csv \
-      --model $model_name \
-      --seq_len $seq_len \
-      --epoch 10 \
-      --n_head 8 >../logs/Pyraformer/Pyraformer_Elec_Custom_04_$pred_len.log
+      -data custom \
+      -input_size 96 \
+      -predict_step $pred_len \
+      -root_path ./dataset/ \
+      -file_name $file_name \
+      -data_path custom_elec_04.csv \
+      -model $model_name \
+      -seq_len $seq_len \
+      -epoch 10 \
+      -n_head 8 >../logs/Pyraformer/Pyraformer_Elec_Custom_04_$pred_len.log
 done
--- scripts/Pyraformer/electricity/Electricity.sh
+++ scripts/Pyraformer/electricity/Electricity.sh
@@ -15,14 +15,14 @@ file_name="Electricity"
 for pred_len in 96 192 384 768
 do
     python -u Pyraformer/long_range_main.py \
-      --data electricity \
-      --input_size 96 \
-      --predict_step $pred_len \
-      --root_path ./dataset/ \
-      --file_name $file_name \
-      --data_path electricity.csv \
-      --model $model_name \
-      --seq_len $seq_len \
-      --epoch 10 \
-      --n_head 8 >../logs/Pyraformer/Pyraformer_ETTh1_$pred_len.log
+      -data electricity \
+      -input_size 96 \
+      -predict_step $pred_len \
+      -root_path ./dataset/ \
+      -file_name $file_name \
+      -data_path electricity.csv \
+      -model $model_name \
+      -seq_len $seq_len \
+      -epoch 10 \
+      -n_head 8 >../logs/Pyraformer/Pyraformer_ETTh1_$pred_len.log
 done
